e8f0b054f8d968186a202c44516d821156a9482a
llucascr <lucasdecamposranzani@gmail.com>
2026-06-17 14:51:00 -0300

    feat: Implementa pipeline de CD

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -20,9 +20,8 @@ import (
 func main() {
 	log.Println("creating a webserver")
 
-	err := godotenv.Load("../../.env")
-	if err != nil {
-		log.Fatal("Error loading .env file")
+	if err := godotenv.Load("../../.env"); err != nil {
+		log.Println("No .env file found, using environment variables")
 	}
 
 	connStr := os.Getenv("DATABASE_URL")
